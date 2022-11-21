45efec66c5ca0496ac893843586c88198594c900
태라 <terra2007@naver.com>
2022-11-21 21:22:06 +0900

    project : 프로젝트 세팅

---
+++ src/main/java/com/example/javastagram/JavastagramApplication.java
@@ -0,0 +1,13 @@
+package com.example.javastagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavastagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javastagram/JavastagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javastagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavastagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
