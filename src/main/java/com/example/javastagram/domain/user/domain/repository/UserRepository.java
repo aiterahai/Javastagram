2152023bd43a6a6b640605d5e2c6bed33cb2d91b
태라 <terra2007@naver.com>
2022-11-27 00:22:26 +0900

    feat : findByAccountId 추가

--- src/main/java/com/example/javastagram/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/javastagram/domain/user/domain/repository/UserRepository.java
@@ -2,9 +2,14 @@ package com.example.javastagram.domain.user.domain.repository;
 
 import com.example.javastagram.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsUserByAccountId(String accountId);
+
+    Optional<User> findUserByAccountId(String accountId);
 }
