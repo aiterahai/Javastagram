fa54219e9c3e8d1bb6d4dd8a0443b80d55f1d199
태라 <terra2007@naver.com>
2022-11-27 01:36:39 +0900

    feat : getCurrentUser 추가

--- src/main/java/com/example/javastagram/domain/user/facade/UserFacade.java
+++ src/main/java/com/example/javastagram/domain/user/facade/UserFacade.java
@@ -4,6 +4,7 @@ import com.example.javastagram.domain.user.domain.User;
 import com.example.javastagram.domain.user.domain.repository.UserRepository;
 import com.example.javastagram.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,6 +12,10 @@ import org.springframework.stereotype.Component;
 public class UserFacade {
 
     private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        return findUserByAccountId(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
 
     public Boolean existsUserByAccountId(String accountId) {
         return userRepository.existsUserByAccountId(accountId);
