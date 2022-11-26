56129c9c5ee167c28ccb90425ddb19814899c8c2
태라 <terra2007@naver.com>
2022-11-27 00:21:27 +0900

    feat : Application 수정

--- src/main/java/com/example/javastagram/JavastagramApplication.java
+++ src/main/java/com/example/javastagram/JavastagramApplication.java
@@ -1,9 +1,14 @@
 package com.example.javastagram;
 
+import com.example.javastagram.global.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableConfigurationProperties(JwtProperties.class)
 public class JavastagramApplication {
 
     public static void main(String[] args) {
