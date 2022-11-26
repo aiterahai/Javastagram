442d0fcc299c5bb8b2061820b733efb34abbf86f
태라 <terra2007@naver.com>
2022-11-27 00:21:46 +0900

    feat : ErrorCode 추가

---
+++ src/main/java/com/example/javastagram/global/error/ErrorCode.java
@@ -0,0 +1,25 @@
+package com.example.javastagram.global.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+
+    EXPIRED_TOKEN(401, "AUTH-401-1", "Token Expired"),
+    INVALID_TOKEN(401, "AUTH-401-2", "Token Invalid"),
+    PASSWORD_NOT_MATCH(401, "USER-401-1", "Password Not Match"),
+
+    USER_NOT_FOUND(404, "USER-404-1", "User Not Found"),
+
+    ACCOUNT_ID_EXISTS(409, "USER-409-1", "Account Id Already Exists"),
+
+    INTERNAL_SERVER_ERROR(500, "SERVER-500-1", "Server Error");
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
