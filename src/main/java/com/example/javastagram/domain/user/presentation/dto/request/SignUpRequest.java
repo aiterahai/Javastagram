a1e9b924cbc6fd76e95e3c292448cc80e4d01611
태라 <terra2007@naver.com>
2022-11-27 00:23:30 +0900

    fix : 정규표현식 수정

--- src/main/java/com/example/javastagram/domain/user/presentation/dto/request/SignUpRequest.java
+++ src/main/java/com/example/javastagram/domain/user/presentation/dto/request/SignUpRequest.java
@@ -15,7 +15,7 @@ public class SignUpRequest {
     private String accountId;
 
     @Size(min = 8, max = 20, message = "비밀번호는 8자 이상, 20자 이하여야 합니다.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]$",
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
             message = "비밀번호는 하나 이상의 문자, 하나 이상의 숫자, 하나 이상의 특수 문자를 포함해야 합니다.")
     private String password;
 }
