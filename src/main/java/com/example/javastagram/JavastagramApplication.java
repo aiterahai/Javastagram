3948b76d4c230b8ad8c4abfcefdfc776dce6248e
태라 <terra2007@naver.com>
2022-11-27 00:36:58 +0900

    feat : @EnableJpaAuditing 추가

--- src/main/java/com/example/javastagram/JavastagramApplication.java
+++ src/main/java/com/example/javastagram/JavastagramApplication.java
@@ -4,10 +4,12 @@ import com.example.javastagram.global.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableJpaAuditing
 @EnableConfigurationProperties(JwtProperties.class)
 public class JavastagramApplication {
 
