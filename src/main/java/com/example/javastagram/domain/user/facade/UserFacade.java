d1856c9b364272c0a1769e9629949b76f3c658da
태라 <terra2007@naver.com>
2022-11-27 00:25:27 +0900

    feat : findUserByAccountId 추가

--- src/main/java/com/example/javastagram/domain/user/facade/UserFacade.java
+++ src/main/java/com/example/javastagram/domain/user/facade/UserFacade.java
@@ -1,6 +1,8 @@
 package com.example.javastagram.domain.user.facade;
 
+import com.example.javastagram.domain.user.domain.User;
 import com.example.javastagram.domain.user.domain.repository.UserRepository;
+import com.example.javastagram.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,5 +14,10 @@ public class UserFacade {
 
     public Boolean existsUserByAccountId(String accountId) {
         return userRepository.existsUserByAccountId(accountId);
+    }
+
+    public User findUserByAccountId(String accountId) {
+        return userRepository.findUserByAccountId(accountId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
     }
 }
