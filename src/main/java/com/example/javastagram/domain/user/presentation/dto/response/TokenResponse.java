c17c4c132a97501b1c91221093165f7540a4a4b5
태라 <terra2007@naver.com>
2022-11-27 00:23:05 +0900

    feat : TokenResponse 생성

---
+++ src/main/java/com/example/javastagram/domain/user/presentation/dto/response/TokenResponse.java
@@ -0,0 +1,11 @@
+package com.example.javastagram.domain.user.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+    private final String accessToken;
+    private final String refreshToken;
+}
