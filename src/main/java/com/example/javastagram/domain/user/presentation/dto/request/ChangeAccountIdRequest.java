9a1088d1840638b3f3f620941f4f11f19c0cd5ff
태라 <terra2007@naver.com>
2022-11-27 14:07:28 +0900

    feat : accountId 정규식 추가

--- src/main/java/com/example/javastagram/domain/user/presentation/dto/request/ChangeAccountIdRequest.java
+++ src/main/java/com/example/javastagram/domain/user/presentation/dto/request/ChangeAccountIdRequest.java
@@ -3,6 +3,7 @@ package com.example.javastagram.domain.user.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -10,5 +11,7 @@ import javax.validation.constraints.Size;
 public class ChangeAccountIdRequest {
 
     @Size(min = 2, max = 30, message = "아이디는 2자 이상, 30자 이하여야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z][0-9a-zA-Z._]{1,29}$",
+            message = "아이디는 영문자로 시작해야 하며 숫자와 영문자, 특수문자 . _ 을 포함할 수 있습니다.")
     private String accountId;
 }
--- src/main/java/com/example/javastagram/domain/user/presentation/dto/request/SignUpRequest.java
+++ src/main/java/com/example/javastagram/domain/user/presentation/dto/request/SignUpRequest.java
@@ -12,6 +12,8 @@ import javax.validation.constraints.Size;
 public class SignUpRequest {
 
     @Size(min = 2, max = 30, message = "아이디는 2자 이상, 30자 이하여야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z][0-9a-zA-Z._]{1,29}$",
+            message = "아이디는 영문자로 시작해야 하며 숫자와 영문자, 특수문자 . _ 을 포함할 수 있습니다.")
     private String accountId;
 
     @Size(min = 8, max = 20, message = "비밀번호는 8자 이상, 20자 이하여야 합니다.")
